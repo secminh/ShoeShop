b9196bebe9b2d8973d9c6785db2ade68a9e4445b
secminh <secminh@gmail.com>
2015-12-15 09:34:21 +0700

    9

--- ShoeShop/Areas/Admin/Models/UserModel.cs
+++ ShoeShop/Areas/Admin/Models/UserModel.cs
@@ -1,21 +1,20 @@
-﻿namespace Model.EF
-{
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
 
-    [Table("User")]
-    public partial class User
+namespace ShoeShop.Areas.Admin.Models
+{
+    public class UserModel
     {
         public long ID { get; set; }
-        
+
         [StringLength(50)]
         [Display(Name = "Tên đăng nhập")]
         [Required(ErrorMessage = "Nhập User name")]
         public string UserName { get; set; }
-        
+
         [StringLength(32)]
         [Display(Name = "Mật khẩu")]
         [Required(ErrorMessage = "Nhập Password")]
@@ -33,8 +32,9 @@
         [Display(Name = "Địa chỉ")]
         public string Address { get; set; }
 
-        
+
         [StringLength(50)]
+        [Required(ErrorMessage = "Nhập User name")]
         [EmailAddress(ErrorMessage = "Mail không hợp lệ")]
         public string Email { get; set; }
 
